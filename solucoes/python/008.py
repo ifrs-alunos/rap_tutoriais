036b807a04959a9cabd99b2431fb96db8c26c88e
Guilherme Alves <ghstr@users.noreply.github.com>
2020-10-02 01:14:57 -0300

    comite

---
+++ solucoes/python/008.py
@@ -0,0 +1,5 @@
+#8. Faça um programa que pede o número de horas de jogatina 
+# de uma pessoa e calcule quanto tempo em dias ela gastou jogando.
+numerodehoras = int(input())
+dias = numerodehoras//60
+print("Voce Gastou jogando:",dias)
---
+++ solucoes/python/011.py
@@ -0,0 +1,7 @@
+#11. Faça um programa que lê um número inteiro e mostra na tela se ele é par ou ímpar. 
+n = int(input()):
+if n%2==0:
+    print('par')
+else:
+    print('ímpar')
+    
